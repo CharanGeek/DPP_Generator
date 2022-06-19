a0c2149f33ca807719cac73203d4e4d892c6a087
CharanGeek <98139183+CharanGeek@users.noreply.github.com>
2022-06-19 10:50:38 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+from MakingPDF import MakePdf
+
+def Welcome():
+    print("----------------------------------------------------------------")
+    print("Thank you for using this script!")
+    print("Made by Aditya Tiwari")
+    print("Follow me on:")
+    print("\tInstagram: uncanny_adi")
+    print("\tDiscord: aawditya #1366")
+    print("----------------------------------------------------------------")
+
+Welcome()
+MakePdf()
